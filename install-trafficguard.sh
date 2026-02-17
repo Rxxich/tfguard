6ef085ce6937780b5e77111b1239c433575e68f7
Rxxich <114252859+Rxxich@users.noreply.github.com>
2026-02-18 00:15:04 +0400

    Update install-trafficguard.sh

--- install-trafficguard.sh
+++ install-trafficguard.sh
@@ -232,7 +232,7 @@ show_menu() {
         echo -e " ${GREEN}2.${NC} 🕵 Логи IPv4 (Live)"
         echo -e " ${GREEN}3.${NC} 🕵 Логи IPv6 (Live)"
         echo -e " ${GREEN}4.${NC} 🧪 Управление IP (Ban/Unban)"
-        echo -e " ${YELLOW}5. 🏳️ Белый список (Whitelist)${NC}" 
+        echo -e " ${GREEN}4.${NC} 🏳️ Белый список (Whitelist)" 
         echo -e " ${GREEN}6.${NC} 🔄 Обновить списки (Update)"
         echo -e " ${GREEN}7.${NC} 🛠️  Переустановить (Reinstall)"
         echo -e " ${RED}8.${NC} 🗑️  Удалить (Uninstall)"
